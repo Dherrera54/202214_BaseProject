a4af362593fd461485dc7f60bbddf45fec367dd6
Dario_herrera <d.herrerag@uniandes.edu.co>
2022-09-14 15:35:12 -0500

    Added logic for member and club

--- src/member/member.module.ts
+++ src/member/member.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MemberEntity } from './member.entity';
+import { MemberService } from './member.service';
 
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([MemberEntity])],
+  providers: [MemberService]
+})
 export class MemberModule {}
