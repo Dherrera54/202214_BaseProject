4681f5c9517737b57b1b5d2358c87c6a127602a6
Dario_herrera <d.herrerag@uniandes.edu.co>
2022-09-15 11:50:09 -0500

    Error handling added to update club description length fix

--- src/club/club.service.ts
+++ src/club/club.service.ts
@@ -29,11 +29,15 @@ export class ClubService {
             return await this.clubRepository.save(club);
     }
     async update(id: string, club: ClubEntity): Promise<ClubEntity> {
+        
         const persistedClub: ClubEntity = await this.clubRepository.findOne({where:{id}});
         if (!persistedClub)
-          throw new BusinessLogicException("The club with the given id was not found", BusinessError.NOT_FOUND);
+        throw new BusinessLogicException("The club with the given id was not found", BusinessError.NOT_FOUND);
         
-        return await this.clubRepository.save({...persistedClub, ...club});
+        if(club.description.length> 100)
+            throw new BusinessLogicException("The club description is over 100 characters", BusinessError.PRECONDITION_FAILED);
+        else   
+            return await this.clubRepository.save({...persistedClub, ...club});
     }
     async delete(id: string) {
         const club: ClubEntity = await this.clubRepository.findOne({where:{id}});
